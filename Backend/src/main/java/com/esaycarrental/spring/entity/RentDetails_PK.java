e40e0c32219c0d949803e51592ee6d631d9288b5
imeshhirushan2117 <imeshhirushan2117@gmail.com>
2023-06-04 15:03:53 +0530

    spring boot

---
+++ Backend/src/main/java/com/esaycarrental/spring/entity/RentDetails_PK.java
@@ -0,0 +1,18 @@
+package com.esaycarrental.spring.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.io.Serializable;
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
+public class RentDetails_PK implements Serializable {
+    private String rentId;
+    private String registrationNumber;
+}
